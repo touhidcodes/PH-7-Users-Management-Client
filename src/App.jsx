1b77e3c8df43d800356b0cf0f413e9e27b137b10
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-11 23:25:36 +0600

    add-user-form

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,31 @@ function App() {
 			.then((res) => res.json())
 			.then((data) => setUsers(data));
 	}, []);
+
+	const handleUser = (event) => {
+		event.preventDefault();
+		const form = event.target;
+		const name = form.name.value;
+		const email = form.email.value;
+		form.reset();
+	};
 	return (
 		<>
 			<h1>Users Management System</h1>
+			<h4>users: {users.length}</h4>
+			<form onSubmit={handleUser}>
+				<input type='text' name='name' id='' placeholder='name' /> <br />
+				<input type='email' name='email' id='' placeholder='email' />
+				<br />
+				<input type='submit' value='Add User' />
+			</form>
+			<div>
+				{users.map((user) => (
+					<p key={user.id}>
+						{user.id} : {user.name}
+					</p>
+				))}
+			</div>
 		</>
 	);
 }
