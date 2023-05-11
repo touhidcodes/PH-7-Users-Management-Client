369e42a6d54b0ef86cb406fe668081ca02af1f16
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-11 23:12:46 +0600

    load-set-data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		fetch("http://localhost:5000/users")
+			.then((res) => res.json())
+			.then((data) => setUsers(data));
+	}, []);
 	return (
 		<>
 			<h1>Users Management System</h1>
