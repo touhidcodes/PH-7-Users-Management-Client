add09c41684dd0ec117e333ff4056e9a536a4f88
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-12 00:38:19 +0600

    post-data

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,22 @@ function App() {
 		const form = event.target;
 		const name = form.name.value;
 		const email = form.email.value;
+		const user = { name, email };
 		form.reset();
+		console.log(name, email);
+
+		fetch("http://localhost:5000/users", {
+			method: "POST",
+			headers: {
+				"content-type": "application/json",
+			},
+			body: JSON.stringify(user),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				const newUser = [...users, user];
+				setUsers(newUser);
+			});
 	};
 	return (
 		<>
